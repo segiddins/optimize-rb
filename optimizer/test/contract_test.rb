14b070fed5cc5212ab8bc5ea5c1fc69158ba3ea0
Samuel Giddins <samuel.giddins@withpersona.com>
2026-04-19 22:50:09 +0900

    Add hardcoded Contract module

---
+++ optimizer/test/contract_test.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+require "test_helper"
+require "ruby_opt/contract"
+
+class ContractTest < Minitest::Test
+  def test_all_five_clauses_are_asserted
+    c = RubyOpt::Contract
+    assert c.no_bop_redefinition?
+    assert c.no_prepend_after_load?
+    assert c.rbs_signatures_truthful?
+    assert c.env_read_only?
+    assert c.no_constant_reassignment?
+  end
+
+  def test_clauses_returns_all_five
+    assert_equal 5, RubyOpt::Contract.clauses.size
+    assert_includes RubyOpt::Contract.clauses, :no_bop_redefinition
+  end
+
+  def test_describe_returns_human_readable_strings
+    text = RubyOpt::Contract.describe
+    assert_kind_of String, text
+    assert_match(/BOP/i, text)
+    assert_match(/prepend/i, text)
+    assert_match(/RBS/, text)
+    assert_match(/ENV/, text)
+    assert_match(/constant/i, text)
+  end
+end
